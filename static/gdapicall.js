d0e4cd2b7a7de8cf465a44355f447903b3461959
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-30 20:49:24 -0800

    messing around with api settings and positioning of imports/CDNs.  r.  Removed LinkedIn login, which stopped working - possibly issue with app set up with LI

---
+++ static/gdapicall.js
@@ -0,0 +1,30 @@
+// TODO: make argument company name
+// Glassdoor
+// sample query
+var GDCompanyAPIQuery = "http://api.glassdoor.com/api/api.htm?t.p=5317&t.k=n07aR34Lk3Y&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q="
+var userAgent ="chrome"
+var userIP="localhost:8000"
+
+
+function gdAPICompany(){
+	apiCall="http://api.glassdoor.com/api/api.htm?t.p="+GDPartner+"&t.k="+GDKey+"&userip="+userIP+"&useragent="+userAgent+"&format=json&v=1&action=jobs-prog&countryId=1&jobTitle=product%20manager"
+	console.log(apiCall);
+	$.ajax({
+		url: apiCall,
+		data: {
+			format: 'json'
+		},
+		dataType: 'jsonp',
+		success: function(data){
+			var gd1 = JSON.stringify(data.response);
+			console.log(gd1);
+			document.getElementById("glassdoor").innerHTML=gd1;
+		},
+		error: function(){
+			console.log("didn't work");
+		},
+		type: 'GET'
+	})
+};
+
+gdAPICompany();
