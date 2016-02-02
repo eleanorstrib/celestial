305943c7ad308faba42b2d712f49e95aeba018d7
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-01 17:00:45 -0800

    partials now work -- had to add ng-route via CDN

--- selesteMain.js
+++ selesteMain.js
@@ -1,4 +1,13 @@
-var app = angular.module('seleste', [])
-	.controller('mainCtrl', [function(){
-		console.log("main ctrl");
-	}]);
+var app = angular.module('seleste', ['ngRoute']);
+
+	app.config(function($routeProvider) {
+		$routeProvider
+		.when('/', {
+			templateUrl: 'static/partials/home.html',
+			controller: 'homeCtrl'
+		});
+	});
+	
+	app.controller('homeCtrl', function($scope) {
+		$scope.message ="it worked!!";
+	})
