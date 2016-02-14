3a8d6a1d670e062d8aa180869272971e0cf9a29f
eleanorstrib <eleanor@sloan.mit.edu>
2016-02-13 20:10:13 -0800

    working on fixing variables - reset company name and api data vars in a different  company name and api data vars in a different place -- mostly works but co name not set properly

--- selesteMain.js
+++ selesteMain.js
@@ -33,23 +33,26 @@ var app = angular.module('seleste', ['ngRoute', 'ngAnimate']);
 				if ($scope.companyArray[i] === ""){
 					$scope.companyArray.pop($scope.companyArray[i]);
 				} else {
+					$scope.coName = '';
+					$scope.gdAPIData = '';
 					$scope.coName = $scope.companyArray[i];
+					console.log('first coName is ' + $scope.coName);
 					// call the api
 					$http.get("http://api.glassdoor.com/api/api.htm?t.p="+GDPartner+"&t.k="+GDKey+"&userip="+userIP + "&useragent="+ userAgent +"&format=json&v=1&action=employers&q=" + $scope.coName)
 						 .then(function(response){
 						 	$scope.gdAPIData = response.data.response.employers;
 						 	if ($scope.gdAPIData.length === 1){
+						 		console.log($scope.coName + " " + $scope.gdAPIData.length );
 								$scope.gdAPICompanyResults.push($scope.gdAPIData);
-								console.log("this is the list of objects after a successful call, one " + $scope.gdAPICompanyResults);
+								console.log($scope.coName, $scope.gdAPIData);
 							} else {
+								console.log($scope.coName + " " + $scope.gdAPIData.length );
 								clarifyQuery($scope.coName, $scope.gdAPIData);
-								console.log(" running clarifyQuery " +  $scope.gdAPIData + $scope.coName);
+								console.log(" running clarifyQuery " + $scope.coName);
 							}
 						});
 				};
 			}
-
-			
 		};
 
 		$scope.reset = function(){
@@ -64,6 +67,6 @@ var app = angular.module('seleste', ['ngRoute', 'ngAnimate']);
 	});
 
 
-	app.controller('aboutCtrl', ['$scope', function($scope) {
-		$scope.pageClass = 'aboutPage';
+	app.controller('prioritiesCtrl', ['$scope', function($scope) {
+		$scope.pageClass = 'prioritiesPage';
 	}]);
